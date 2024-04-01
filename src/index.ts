41a4077ee51683581bb20c59aefe3c4a67218c5e
Joshua Zacek <personal.joshuazacek@gmail.com>
2024-04-01 20:50:08 +0100

    Added PostgreSQL DB

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // Libraries
 import express from "express";
+import "dotenv/config";
+import { db } from "./db/index.js";
 
 // Express Config
 const app = express();
@@ -9,6 +11,10 @@ const port = 8080;
 app.get("/index", (req, res) => {
   res.send("Hello, World!");
 });
+
+// Connect to PostgreSQL database
+await db.connect();
+console.log("Database Connection Successful!");
 
 // Start HTTP Server
 app.listen(port, () => {
