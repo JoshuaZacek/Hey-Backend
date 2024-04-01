37c7ad2dff9520d7b98889bd160c1fde6e73a4b2
Joshua Zacek <personal.joshuazacek@gmail.com>
2024-04-01 15:28:48 +0100

    Intialized Express.js HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// Libraries
+import express from "express";
+
+// Express Config
+const app = express();
+const port = 8080;
+
+// Routes
+app.get("/index", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start HTTP Server
+app.listen(port, () => {
+  console.log(`Started Hey Backend. Port: ${port}`);
+});
